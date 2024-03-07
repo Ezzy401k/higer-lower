7311b410a4af444b87f035d3177b9fe61a629bbd
Esrael Mekdem <esraelmekdem@gmail.com>
2024-03-07 14:45:31 +0300

    added game loop

--- higer-lower.py
+++ higer-lower.py
@@ -16,32 +16,42 @@ def compare(item1, item2):
         else:
             return 'B'
         
-score = 0
-loop = True
-while loop:
 
-    item1 = fetch_data()
-    item2 = fetch_data()
+new_game =True
 
+while new_game:
+    score = 0
+    loop = True
+    while loop:
 
+        item1 = fetch_data()
+        item2 = fetch_data()
 
-    handler = True
-    while handler:
+        handler = True
+        while handler:
 
-        print(f"Compare A: {item1['name']}, {item1['description']}, from {item1["country"]}.")
-        print(f"Against B: {item2['name']}, {item2['description']}, from {item2["country"]}.")
-        users_input = input("Who has more followers? Type 'A' or 'B': ").upper()
-        os.system("cls")
-        if users_input == 'A' or users_input == 'B':
-            
-            if users_input == compare(item1, item2):
-                score += 1
-                handler = False
-                print(f"Your rigth! Current score: {score}")
+            print(f"Compare A: {item1['name']}, {item1['description']}, from {item1["country"]}.")
+            print(f"Against B: {item2['name']}, {item2['description']}, from {item2["country"]}.")
+            users_input = input("Who has more followers? Type 'A' or 'B': ").upper()
+            os.system("cls")
+            if users_input == 'A' or users_input == 'B':
+                
+                if users_input == compare(item1, item2):
+                    score += 1
+                    handler = False
+                    print(f"Your rigth! Current score: {score}")
+                else:
+                    loop = False
+                    handler = False
+                    print(f"Sorry, that's wrong. Final score: {score}")
             else:
-                loop = False
-                handler = False
-                print(f"Sorry, that's wrong. Final score: {score}")
-        else:
-            print("Please Enter 'A' or 'B'.")
+                print("Please Enter 'A' or 'B'.")
+    again = input("Do you want to play again,\ninput 'yes' or 'no': ")
+
+    if again == 'yes':
+        os.system('cls')
+    else:
+        new_game = False
+input("Tap Enter to Exit!")
+
 
