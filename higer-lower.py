fdc67ed4a536335d5bfc35b7e4c541af96bebf3d
Esrael Mekdem <esraelmekdem@gmail.com>
2024-03-07 14:17:17 +0300

    error handler added

--- higer-lower.py
+++ higer-lower.py
@@ -3,38 +3,45 @@ import game_data
 data = game_data.data
 import os
 
-# def fetch_data():
-#     item = {}
-#     item = data[random.randint(0,len(data)-1)]
-#     return item
+def fetch_data():
+    item = {}
+    item = data[random.randint(0,len(data)-1)]
+    return item
 
-# def compare(item1, item2):
-#         num1 = item1['follower_count'] 
-#         num2 = item2['follower_count']
-#         if num1 > num2:
-#             return 'A'
-#         else:
-#             return 'B'
-# score = 0
-# loop = True
-# while loop:
+def compare(item1, item2):
+        num1 = item1['follower_count'] 
+        num2 = item2['follower_count']
+        if num1 > num2:
+            return 'A'
+        else:
+            return 'B'
+        
+score = 0
+loop = True
+while loop:
 
-#     item1 = fetch_data()
-#     item2 = fetch_data()
-
-#     print(f"Compare A: {item1['name']}, {item1['description']}, from {item1["country"]}.")
-#     print(f"Against B: {item2['name']}, {item2['description']}, from {item2["country"]}.")
-
-#     users_input = input("Who has more followers? Type 'A' or 'B': ")
-#     os.system("cls")
-
-#     if users_input == compare(item1, item2):
-#         score += 1
-#         print(f"Your rigth! Current score: {score}")
-#     else:
-#         loop = False
-#         print(f"Sorry, that's wrong. Final score: {score}")
+    item1 = fetch_data()
+    item2 = fetch_data()
 
 
-for i in range(len(data) - 1):
-    print(data[i]["follower_count"])
+
+    handler = True
+    while handler:
+
+        print(f"Compare A: {item1['name']}, {item1['description']}, from {item1["country"]}.")
+        print(f"Against B: {item2['name']}, {item2['description']}, from {item2["country"]}.")
+        users_input = input("Who has more followers? Type 'A' or 'B': ").upper()
+        os.system("cls")
+        if users_input == 'A' or users_input == 'B':
+            
+            if users_input == compare(item1, item2):
+                score += 1
+                handler = False
+                print(f"Your rigth! Current score: {score}")
+            else:
+                loop = False
+                handler = False
+                print(f"Sorry, that's wrong. Final score: {score}")
+        else:
+            print("Please Enter 'A' or 'B'.")
+
