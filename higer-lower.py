f25578f11c7bd989f23ac1ec08a8dfedd7d58c4d
Esrael Mekdem <esraelmekdem@gmail.com>
2024-03-07 14:09:34 +0300

    functon placment

--- higer-lower.py
+++ higer-lower.py
@@ -3,37 +3,38 @@ import game_data
 data = game_data.data
 import os
 
-def fetch_data():
-    item = {}
-    item = data[random.randint(0,len(data)-1)]
-    return item
+# def fetch_data():
+#     item = {}
+#     item = data[random.randint(0,len(data)-1)]
+#     return item
 
-score = 0
-loop = True
-while loop:
+# def compare(item1, item2):
+#         num1 = item1['follower_count'] 
+#         num2 = item2['follower_count']
+#         if num1 > num2:
+#             return 'A'
+#         else:
+#             return 'B'
+# score = 0
+# loop = True
+# while loop:
 
-    item1 = fetch_data()
-    item2 = fetch_data()
+#     item1 = fetch_data()
+#     item2 = fetch_data()
 
-    def compare(item1, item2):
-        num1 = item1['follower_count'] 
-        num2 = item2['follower_count']
-        if num1 > num2:
-            return 'A'
-        else:
-            return 'B'
+#     print(f"Compare A: {item1['name']}, {item1['description']}, from {item1["country"]}.")
+#     print(f"Against B: {item2['name']}, {item2['description']}, from {item2["country"]}.")
 
-    print(f"Compare A: {item1['name']}, {item1['description']}, from {item1["country"]}.")
-    print(f"Against B: {item2['name']}, {item2['description']}, from {item2["country"]}.")
+#     users_input = input("Who has more followers? Type 'A' or 'B': ")
+#     os.system("cls")
 
-    users_input = input("Who has more followers? Type 'A' or 'B': ")
-    os.system("cls")
-
-    if users_input == compare(item1, item2):
-        score += 1
-        print(f"Your rigth! Current score: {score}")
-    else:
-        loop = False
-        print(f"Sorry, that's wrong. Final score: {score}")
+#     if users_input == compare(item1, item2):
+#         score += 1
+#         print(f"Your rigth! Current score: {score}")
+#     else:
+#         loop = False
+#         print(f"Sorry, that's wrong. Final score: {score}")
 
 
+for i in range(len(data) - 1):
+    print(data[i]["follower_count"])
